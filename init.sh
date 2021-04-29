e4aa2f68a885a6e3e84dadfee3929941136a80d7
RoyBg <Roybargil@gmail.com>
2021-04-29 16:02:14 +0300

    fixed worker container

--- init.sh
+++ init.sh
@@ -3,7 +3,11 @@ sudo docker build -t celery_mgmt -f AWS/celereyMGMT/Dockerfile . ;
 #create the worker img
 sudo docker build -t celery_worker -f AWS/celereyWorker/Dockerfile . ;
 
-# sudo docker run -i celery_mgmt
+# sudo docker run -i celery_mgmt -p 5000:5000/tcp
 
 # sudo docker run -i celery_worker
+
+
+
+
 
